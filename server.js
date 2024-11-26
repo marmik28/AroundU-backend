5412e9afb5a1d77c1513e03c373206efb926066f
Marmik patel <62463824+marmik28@users.noreply.github.com>
2024-11-26 16:31:14 -0500

    bug fixes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 const allowedOrigins = [
-  'https://aroundu-kohl.vercel.app/',
+  'https://aroundu-kohl.vercel.app',
   'http://localhost:3000',
 ];
 
